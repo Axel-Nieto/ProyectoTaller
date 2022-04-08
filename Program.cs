0972eba45b518914b38284537359df3b81868af2
Blackwing <blackwingoficial@gmail.com>
2022-04-08 20:34:16 -0300

    New API metod

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoTaller.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +9,23 @@ namespace ProyectoTaller
 {
     static class Program
     {
+        
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
-        [STAThread]
+        //[STAThread]
         static void Main()
         {
+            /*
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
+            */
+
+            ApiServices api = new ApiServices();
+
+            api.GetItem("Leg");
         }
+        
     }
 }
